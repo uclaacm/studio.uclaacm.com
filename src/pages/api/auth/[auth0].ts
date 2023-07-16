81fe94aedeb5a6a7b8770f0466c2281a846adb42
ketexon <zane.a.s.clark@gmail.com>
2023-07-15 20:11:42 -0700

    added debug print to auth handler

--- src/pages/api/auth/[auth0].ts
+++ src/pages/api/auth/[auth0].ts
@@ -1,3 +1,9 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+const handler = handleAuth();
+
+export default async function Auth(req: NextApiRequest, res: NextApiResponse) {
+	console.log(process.env)
+	handler(req, res);
+}
