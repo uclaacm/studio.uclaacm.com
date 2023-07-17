10d7d1273b72a5f7367e8b3336d75479a2f02cc1
ketexon <zane.a.s.clark@gmail.com>
2023-07-16 19:29:46 -0700

    removed log from handle auth

--- src/pages/api/auth/[auth0].ts
+++ src/pages/api/auth/[auth0].ts
@@ -1,9 +1,4 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-const handler = handleAuth();
-
-export default async function Auth(req: NextApiRequest, res: NextApiResponse) {
-	console.log(process.env)
-	handler(req, res);
-}
+export default handleAuth();
