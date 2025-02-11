089b110599ae7313351475521d1de389cae964a0
Aubrey Clark (ketexon) <zane.a.s.clark@gmail.com>
2025-02-10 19:32:56 -0800

    added peakemon

--- src/pages/games/connections/games.ts
+++ src/pages/games/connections/games.ts
@@ -22,6 +22,88 @@ export type Game = InternalGame | ExternalGame;
 
 const gamesUnsorted: Game[] = [
 	{
+		name: `Peak\u00E9mon`,
+		date: "10 Feb 2025",
+		author: "Matthew Workman",
+		categories: [
+			{
+				name: "Legendary Pokemon",
+				difficulty: 1,
+				entries: ["Mewtwo", "Okidogi", "Rayquaza", "Calyrex"],
+			},
+			{
+				name: "Bug Type Pokemon",
+				difficulty: 2,
+				entries: ["Galvantula", "Orbeetle", "Grubbin", "Centiskorch"],
+			},
+			{
+				name: "Champion Aces",
+				difficulty: 3,
+				entries: ["Volcarona", "Metagross", "Garchomp", "Kingambit"],
+			},
+			{
+				name: "Pokemon used by Hatsune Miku Crossover",
+				difficulty: 4,
+				entries: ["Flygon", "Jirachi", "Obstagoon", "Rotom"],
+			},
+		]
+	},
+	{
+		name: "Extreme Demon",
+		date: "26 January 2025",
+		author: "Zandy Zhao",
+		categories: [
+			{
+				name: "NPC TURNS CURRENCY INTO STRENGTH",
+				difficulty: 1,
+				entries: ["BLOODBORNE", "DARK SOULS II", "DARK SOULS III", "ELDEN RING"],
+			},
+			{
+				name: "CONTAINS A BOSS THAT'S VERY EASILY PARRIABLE",
+				difficulty: 2,
+				entries: ["DARK SOULS", "MGR REVENGENCE", "SEKIRO", "SPACE MARINE 2"],
+			},
+			{
+				name: "GAMES WITH A SWORDSMAN AS ONE OF THE BEST CHARACTERS",
+				difficulty: 3,
+				entries: ["BALDUR'S GATE 3", "BATMAN: ARKHAM KNIGHT", "DARKEST DUNGEON", "RUINED KING"],
+			},
+			{
+				name: "GAMES THAT CAN BE VERY FRUSTRATING TO PLAY DEPENDING ON YOUR TEAMMATES",
+				difficulty: 4,
+				entries: ["MARVEL RIVALS", "BRAWL STARS", "TITANFALL 2", "DEEP ROCK GALACTIC"],
+			},
+		],
+	},
+	{
+		name: "Word",
+		date: "12 January 2025",
+		author: "Aubrey Clark",
+		categories: [
+			{
+				name: "Ore in Minecraft",
+				difficulty: 1,
+				entries: ["Copper", "Gold", "Emerald", "Diamond"],
+			},
+			{
+				name: "Pokemon Games",
+				difficulty: 2,
+				entries: ["X", "Shield", "White", "Red"],
+			},
+			{
+				name: "Words in Legend of Zelda Games",
+				difficulty: 3,
+				entries: ["Sword", "Wind", "Breath", "Tears"],
+			},
+			{
+				name: "First Words in 2024 Game Awards Winners",
+				difficulty: 4,
+				entries: ["Arkham", "Metaphor", "Black", "Astro"],
+			},
+
+		],
+	},
+	{
 		name: "Enemy",
 		date: "27 Jan 2025",
 		author: "Aubrey Clark",
