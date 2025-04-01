3057eac4bcb3ebfd93cbd0bd5510da49353e3e9a
ketexon <zane.a.s.clark@gmail.com>
2025-03-31 18:00:41 -0700

    added template for home page

---
+++ src/pages/home/Template/index.tsx
@@ -0,0 +1,30 @@
+import { Box } from "@mui/material";
+import { useInView } from "framer-motion";
+import React from "react";
+import { HomeSectionProps } from "../../index.page";
+
+export default function Template(props: HomeSectionProps) {
+  const { setActive, id } = props;
+
+  const root = React.useRef<HTMLDivElement>(undefined);
+  const inView = useInView(root, { margin: "-64px" });
+  React.useEffect(() => {
+    if (inView) {
+      setActive();
+    }
+  }, [inView]);
+
+
+  return (
+    <Box
+      ref={root}
+      id={id}
+      sx={{
+        width: "100%",
+        minHeight: "100vh",
+        scrollSnapAlign: "start",
+      }}
+    >
+    </Box>
+  );
+}
